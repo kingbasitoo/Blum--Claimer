674b0fc8c26f0fb332e98171b0a6ec98e931b669
Bernard.O <114490070+BernardOnuh@users.noreply.github.com>
2024-07-08 10:45:23 +0100

    Update config.py
    Commit

--- data/config.py
+++ data/config.py
@@ -2,38 +2,31 @@
 API_ID = 11111111
 API_HASH = 'Your api hash'
 
-# задержка между подключениями к аккаунтам
+# Delay between connections to accounts
 ACC_DELAY = [5, 15]
 
-# тип прокси
-PROXY_TYPE = "socks5" # http/socks5
+# Proxy type
+PROXY_TYPE = "socks5"  # http/socks5
 
-# папка с сессиями (не менять)
+# Session folder (do not change)
 WORKDIR = "sessions/"
 
-# использование прокси
-USE_PROXY = True # True/False
+# Use proxy
+USE_PROXY = True  # True/False
 
-# скок поинтов с игры
-POINTS = [100, 140] #[min, max]
+# Points range from game
+POINTS = [100, 140]  # [min, max]
 
-# тратить алмазы
-SPEND_DIAMONDS = True # True/False
+# Spend diamonds
+SPEND_DIAMONDS = True  # True/False
 
-# сон между играми
-SLEEP_GAME_TIME = [100,200] #[min,max]
+# Sleep time between games
+SLEEP_GAME_TIME = [100, 200]  # [min, max]
 
-# мини задержки
-MINI_SLEEP = [3,7] #[min,max]
+# Mini sleep delay
+MINI_SLEEP = [3, 7]  # [min, max]
 
-# доп задержка после 8часов 
-SLEEP_8HOURS = [60*60,120*60] #[min,max] seconds
+# Additional delay after 8 hours
+SLEEP_8HOURS = [60 * 60, 120 * 60]  # [min, max] seconds
 
-hello ='''              _                               __  _        
- _ __    ___ | |_  _   _   __ _  ___   ___   / _|| |_  ___ 
-| '_ \  / _ \| __|| | | | / _` |/ __| / _ \ | |_ | __|/ __|
-| |_) ||  __/| |_ | |_| || (_| |\__ \| (_) ||  _|| |_ \__ \\
-| .__/  \___| \__| \__, | \__,_||___/ \___/ |_|   \__||___/
-|_|                |___/        
-
-'''
+hello = Welcome
